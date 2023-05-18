835b3c54f7abf4ee32d28992b10720a58c779e98
AyushIyankan <ayushiyankan007@gmail.com>
2023-05-19 00:45:47 +0530

    fix render questions on profile page

--- src/components/cards/questionLarge/index.tsx
+++ src/components/cards/questionLarge/index.tsx
@@ -1,77 +1,81 @@
-import { Question } from '@/types';
-import axios from 'axios';
-import Link from 'next/link';
-import { useQuery } from 'wagmi';
+import { Question } from "@/types";
+import axios from "axios";
+import Link from "next/link";
+import { useQuery } from "wagmi";
 
-const QuestionCardLarge = (props: Question) => {
-  const { id, downvotes, upvotes, uri } = props;
+const QuestionCardLarge = ({ question }: Question) => {
+    const { id, downvotes, upvotes, uri } = question;
 
-  const { data, isError, isLoading } = useQuery(
-    ['get-question-metadata', id?.toString()],
-    () => axios.get(uri)
-  );
-
-  if (isLoading) {
-    return (
-      <Wrapper
-        id={id?.toString()}
-        voteCount={upvotes?.sub(downvotes).toNumber()}>
-        <div>Loading...</div>
-      </Wrapper>
+    const { data, isError, isLoading } = useQuery(
+        ["get-question-metadata", id?.toString()],
+        () => axios.get(uri)
     );
-  }
 
-  if (isError) {
+    if (isLoading) {
+        return (
+            <Wrapper
+                id={id?.toString()}
+                voteCount={upvotes?.sub(downvotes).toNumber()}>
+                <div>Loading...</div>
+            </Wrapper>
+        );
+    }
+
+    if (isError) {
+        return (
+            <Wrapper
+                id={id?.toString()}
+                voteCount={upvotes?.sub(downvotes).toNumber()}>
+                <div>Something went wrong!</div>
+            </Wrapper>
+        );
+    }
+
+    if (!data) {
+        return (
+            <Wrapper
+                id={id?.toString()}
+                voteCount={upvotes?.sub(downvotes).toNumber()}>
+                <div>No data found.</div>
+            </Wrapper>
+        );
+    }
+
+    const metadata = data.data as any;
+
     return (
-      <Wrapper
-        id={id?.toString()}
-        voteCount={upvotes?.sub(downvotes).toNumber()}>
-        <div>Something went wrong!</div>
-      </Wrapper>
+        <Link
+            href={`/${id?.toString()}`}
+            className="cursor-pointer [border:none] p-6 bg-gray-500 rounded-xl w-full gap-x-8 flex flex-row box-border items-center justify-start text-white no-underline">
+            <div className="text-sm leading-5 text-silver-100">
+                {upvotes?.sub(downvotes).toString()} Votes
+            </div>
+            <p className="text-base leading-6 text-gainsboro">
+                {metadata?.title}
+            </p>
+        </Link>
     );
-  }
-
-  if (!data) {
-    return (
-      <Wrapper
-        id={id?.toString()}
-        voteCount={upvotes?.sub(downvotes).toNumber()}>
-        <div>No data found.</div>
-      </Wrapper>
-    );
-  }
-
-  const metadata = data.data as any;
-
-  return (
-    <Link
-      href={`/${id?.toString()}`}
-      className='cursor-pointer [border:none] p-6 bg-gray-500 rounded-xl w-full gap-x-8 flex flex-row box-border items-center justify-start text-white no-underline'>
-      <div className='text-sm leading-5 text-silver-100'>
-        {upvotes?.sub(downvotes).toString()} Votes
-      </div>
-      <p className='text-base leading-6 text-gainsboro'>{metadata?.title}</p>
-    </Link>
-  );
 };
 
 export default QuestionCardLarge;
 
 const Wrapper = ({
-  children,
-  voteCount,
-  id,
+    children,
+    voteCount,
+    id,
 }: {
-  children: JSX.Element;
-  voteCount: number;
-  id: string;
+    children: JSX.Element;
+    voteCount: number;
+    id: string;
 }) => {
-  return (
-    <Link
-      href={`/${id?.toString()}`}
-      className='cursor-pointer [border:none] p-6 bg-gray-500 rounded-xl w-full gap-x-8 flex flex-row box-border items-center justify-start text-white no-underline'>
-      <div className='text-sm leading-5 text-silver-100'>{voteCount} Votes</div>
-      {children}
-    </Link>
-  );
+    return (
+        <Link
+            href={`/${id?.toString()}`}
+            className="cursor-pointer [border:none] p-6 bg-gray-500 rounded-xl w-full gap-x-8 flex flex-row box-border items-center justify-start text-white no-underline">
+            <div className="text-sm leading-5 text-silver-100">
+                {voteCount} Votes
+            </div>
+            {children}
+        </Link>
+    );
 };
