19f54431c8c63fe22d8b49d0ee55fa453865531f
AyushIyankan <ayushiyankan007@gmail.com>
2023-05-19 01:46:42 +0530

    add answer types, fix answers in profile (tyoes may change)

---
+++ src/components/cards/answerLarge/index.tsx
@@ -0,0 +1,81 @@
+import { Answer } from "@/types";
+import axios from "axios";
+import Link from "next/link";
+import { useQuery } from "wagmi";
+
+const AnswerCardLarge = ({ answer }: Answer) => {
+    const { id, downvotes, upvotes, uri } = answer;
+
+    const { data, isError, isLoading } = useQuery(
+        ["get-question-metadata", id?.toString()],
+        () => axios.get(uri)
+    );
+
+    if (isLoading) {
+        return (
+            <Wrapper
+                id={id?.toString()}
+                voteCount={upvotes?.sub(downvotes).toNumber()}>
+                <div>Loading...</div>
+            </Wrapper>
+        );
+    }
+
+    if (isError) {
+        return (
+            <Wrapper
+                id={id?.toString()}
+                voteCount={upvotes?.sub(downvotes).toNumber()}>
+                <div>Something went wrong!</div>
+            </Wrapper>
+        );
+    }
+
+    if (!data) {
+        return (
+            <Wrapper
+                id={id?.toString()}
+                voteCount={upvotes?.sub(downvotes).toNumber()}>
+                <div>No data found.</div>
+            </Wrapper>
+        );
+    }
+
+    const metadata = data.data as any;
+
+    return (
+        <Link
+            href={`/${id?.toString()}`}
+            className="cursor-pointer [border:none] p-6 bg-gray-500 rounded-xl w-full gap-x-8 flex flex-row box-border items-center justify-start text-white no-underline">
+            <div className="text-sm leading-5 text-silver-100">
+                {upvotes?.sub(downvotes).toString()} Votes
+            </div>
+            <p className="text-base leading-6 text-gainsboro">
+                {metadata?.title}
+            </p>
+        </Link>
+    );
+};
+
+export default AnswerCardLarge;
+
+const Wrapper = ({
+    children,
+    voteCount,
+    id,
+}: {
+    children: JSX.Element;
+    voteCount: number;
+    id: string;
+}) => {
+    return (
+        <Link
+            href={`/${id?.toString()}`}
+            className="cursor-pointer [border:none] p-6 bg-gray-500 rounded-xl w-full gap-x-8 flex flex-row box-border items-center justify-start text-white no-underline">
+            <div className="text-sm leading-5 text-silver-100">
+                {voteCount} Votes
+            </div>
+            {children}
+        </Link>
+    );
+};
--- src/components/stats/index.tsx
+++ src/components/stats/index.tsx
@@ -9,7 +9,7 @@ import {
 import QuestionCardLarge from "../cards/questionLarge";
 import { StatsProps } from "@/types/profile";
 
-const Stats = ({ questions }: StatsProps) => {
+const Stats = ({ questions, answers }: StatsProps) => {
     return (
         <div className="bg-gray-100 rounded-xl p-6 lg:p-10 text-white">
             <div className="mb-12">
@@ -23,41 +23,44 @@ const Stats = ({ questions }: StatsProps) => {
                         ({questions?.length} Questions)
                     </span>
                 </h2>
-                {questions?.map((question) => {
-                    return (
-                        <div
-                            className="m-0 mb-3"
-                            key={Number(question.id?._hex)}>
-                            <QuestionCardLarge question={question} />
-                        </div>
-                    );
-                })}
+                {questions.length === 0 ? (
+                    <p className="text-lg">No Questions found</p>
+                ) : (
+                    <>
+                        {questions?.map((question) => {
+                            return (
+                                <div
+                                    className="m-0 mb-3"
+                                    key={Number(question.id?._hex)}>
+                                    <QuestionCardLarge question={question} />
+                                </div>
+                            );
+                        })}
+                    </>
+                )}
             </div>
             <div className="">
                 <h2 className="m-0 mb-6 text-[28px] flex flex-col lg:flex-row gap-y-10">
                     Answers{" "}
                     <span className="text-silver-100 text-base">
-                        (3 Answers)
+                        ({answers?.length} Answers)
                     </span>
                 </h2>
-                <div className="m-0 mb-3">
-                    <QuestionCardLarge
-                        question="How can I anchor my absolute button to the end of a div/container so that when I scroll that div/container to right it will remain in the same place"
-                        voteCount={-5}
-                    />
-                </div>
-                <div className="m-0 mb-3">
-                    <QuestionCardLarge
-                        question="How can I anchor my absolute button to the end of a div/container so that when I scroll that div/container to right it will remain in the same place"
-                        voteCount={-5}
-                    />
-                </div>
-                <div className="m-0 mb-3">
-                    <QuestionCardLarge
-                        question="How can I anchor my absolute button to the end of a div/container so that when I scroll that div/container to right it will remain in the same place"
-                        voteCount={-5}
-                    />
-                </div>
+                {answers.length === 0 ? (
+                    <p className="text-lg">No answers found</p>
+                ) : (
+                    <>
+                        {answers?.map((answer) => {
+                            return (
+                                <div
+                                    className="m-0 mb-3"
+                                    key={Number(answer.id?._hex)}>
+                                    <QuestionCardLarge answer={answer} />
+                                </div>
+                            );
+                        })}
+                    </>
+                )}
             </div>
         </div>
     );
