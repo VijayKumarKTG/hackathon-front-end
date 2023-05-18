58da941743364980779094a3129d7c19934e785f
AyushIyankan <ayushiyankan007@gmail.com>
2023-05-18 20:55:11 +0530

    add fixes

--- src/app/questions/page.tsx
+++ src/app/questions/page.tsx
@@ -1,172 +1,177 @@
 /* eslint-disable @next/next/no-img-element */
-'use client';
-import { useEffect } from 'react';
-import type { NextPage } from 'next';
-import Link from 'next/link';
-import { useContractRead, useContractReads } from 'wagmi';
-import { BigNumber } from 'ethers';
+"use client";
+import { useEffect } from "react";
+import type { NextPage } from "next";
+import Link from "next/link";
+import { useContractRead, useContractReads } from "wagmi";
+import { BigNumber } from "ethers";
 
-import RelatedQuestions from '@/components/relatedQuestions';
-import TrendingTags from '@/components/trendingTags';
-import QuestionCardLarge from '@/components/cards/questionLarge';
-import { get_all_questions_by_id, get_total_counts } from '@/abi/social';
-import { Address, Question } from '@/types';
-import usePaginationStore from '@/store/pagination';
+import RelatedQuestions from "@/components/relatedQuestions";
+import TrendingTags from "@/components/trendingTags";
+import QuestionCardLarge from "@/components/cards/questionLarge";
+import { get_all_questions_by_id, get_total_counts } from "@/abi/social";
+import { Address, Question } from "@/types";
+import usePaginationStore from "@/store/pagination";
 
 const Questions: NextPage = () => {
-  const {
-    currentPage,
-    totalItems,
-    pageSize,
-    totalPages,
-    items,
-    setCurrentPage,
-    setTotalItems,
-    setTotalPages,
-    setItems,
-  } = usePaginationStore();
+    const {
+        currentPage,
+        totalItems,
+        pageSize,
+        totalPages,
+        items,
+        setCurrentPage,
+        setTotalItems,
+        setTotalPages,
+        setItems,
+    } = usePaginationStore();
 
-  const {
-    data: totalCounts,
-    isLoading: isCountLoading,
-    isError: isCountError,
-    refetch: refetchCount,
-  } = useContractRead({
-    address: process.env.NEXT_PUBLIC_STACK3_ADDRESS as Address,
-    abi: get_total_counts,
-    functionName: 'getTotalCounts',
-    enabled: false,
-  });
+    const {
+        data: totalCounts,
+        isLoading: isCountLoading,
+        isError: isCountError,
+        refetch: refetchCount,
+    } = useContractRead({
+        address: process.env.NEXT_PUBLIC_STACK3_ADDRESS as Address,
+        abi: get_total_counts,
+        functionName: "getTotalCounts",
+        enabled: false,
+    });
 
-  useEffect(() => {
-    refetchCount();
-  }, []);
+    useEffect(() => {
+        refetchCount();
+    }, []);
 
-  useEffect(() => {
-    if (totalCounts && !isCountLoading && !isCountError) {
-      const count_list: BigNumber[] = totalCounts as BigNumber[];
-      const temp_count = count_list[1].toNumber();
+    useEffect(() => {
+        if (totalCounts && !isCountLoading && !isCountError) {
+            const count_list: BigNumber[] = totalCounts as BigNumber[];
+            const temp_count = count_list[1].toNumber();
 
-      setTotalItems(temp_count);
-      setTotalPages(
-        temp_count < pageSize ? 1 : Math.ceil(temp_count / pageSize)
-      );
+            setTotalItems(temp_count);
+            setTotalPages(
+                temp_count < pageSize ? 1 : Math.ceil(temp_count / pageSize)
+            );
+        }
+    }, [totalCounts]);
+
+    useEffect(() => {
+        if (totalItems > 0) {
+            let startIndex = 0;
+            let endIndex = 0;
+            const items_list: number[] = [];
+
+            if (totalPages === 1) {
+                startIndex = 0;
+                endIndex = totalItems;
+            } else {
+                startIndex = (totalPages - currentPage) * 10;
+                endIndex = startIndex + 10;
+            }
+
+            for (let i = endIndex; i > startIndex; i--) {
+                items_list.push(i);
+            }
+
+            setItems(items_list);
+        }
+    }, [totalItems, currentPage]);
+
+    const contract = {
+        address: process.env.NEXT_PUBLIC_STACK3_ADDRESS as Address,
+        abi: get_all_questions_by_id,
+        functionName: "getQuestionById",
+    };
+
+    const {
+        data: questions,
+        isLoading: isQuestionsLoading,
+        isError: isQuestionsError,
+        refetch: refetchQuestions,
+    } = useContractReads({
+        contracts: items?.map((id: number) => ({
+            ...contract,
+            args: [id],
+        })) as any,
+        enabled: false,
+    });
+
+    let questions_list: Question[] = questions as Question[];
+
+    useEffect(() => {
+        if (items.length > 0) {
+            refetchQuestions();
+        }
+    }, [items]);
+
+    if (isCountLoading || isQuestionsLoading) {
+        return (
+            <Wrapper>
+                <div>Loading...</div>
+            </Wrapper>
+        );
     }
-  }, [totalCounts]);
 
-  useEffect(() => {
-    if (totalItems > 0) {
-      let startIndex = 0;
-      let endIndex = 0;
-      const items_list: number[] = [];
-
-      if (totalPages === 1) {
-        startIndex = 0;
-        endIndex = totalItems;
-      } else {
-        startIndex = (totalPages - currentPage) * 10;
-        endIndex = startIndex + 10;
-      }
-
-      for (let i = endIndex; i > startIndex; i--) {
-        items_list.push(i);
-      }
-
-      setItems(items_list);
+    if (isCountError || isQuestionsError) {
+        return (
+            <Wrapper>
+                <div>Something went wrong.</div>
+            </Wrapper>
+        );
     }
-  }, [totalItems, currentPage]);
 
-  const contract = {
-    address: process.env.NEXT_PUBLIC_STACK3_ADDRESS as Address,
-    abi: get_all_questions_by_id,
-    functionName: 'getQuestionById',
-  };
-
-  const {
-    data: questions,
-    isLoading: isQuestionsLoading,
-    isError: isQuestionsError,
-    refetch: refetchQuestions,
-  } = useContractReads({
-    contracts: items?.map((id: number) => ({ ...contract, args: [id] })) as any,
-    enabled: false,
-  });
-
-  let questions_list: Question[] = questions as Question[];
-
-  useEffect(() => {
-    if (items.length > 0) {
-      refetchQuestions();
+    if (items.length === 0 || questions_list?.length === 0) {
+        return (
+            <Wrapper>
+                <div className="text-lg text-white">No questions to show.</div>
+            </Wrapper>
+        );
     }
-  }, [items]);
 
-  if (isCountLoading || isQuestionsLoading) {
+    console.log(items);
+
     return (
-      <Wrapper>
-        <div>Loading...</div>
-      </Wrapper>
+        <Wrapper>
+            <>
+                <div className="text-[24px] leading-6 mb-4 font-medium text-silver-100">
+                    {totalItems} Questions
+                </div>
+
+                {questions_list?.map((question: Question) => (
+                    <div className="m-0 mb-3" key={question?.id.toString()}>
+                        <QuestionCardLarge {...question} />
+                    </div>
+                ))}
+            </>
+        </Wrapper>
     );
-  }
-
-  if (isCountError || isQuestionsError) {
-    return (
-      <Wrapper>
-        <div>Something went wrong.</div>
-      </Wrapper>
-    );
-  }
-
-  if (items.length === 0 || questions_list?.length === 0) {
-    return (
-      <Wrapper>
-        <div>No questions to show.</div>
-      </Wrapper>
-    );
-  }
-
-  return (
-    <Wrapper>
-      <>
-        <div className='text-[24px] leading-6 mb-4 font-medium text-silver-100'>
-          {totalItems} Questions
-        </div>
-
-        {questions_list?.map((question: Question) => (
-          <div className='m-0 mb-3' key={question?.id.toString()}>
-            <QuestionCardLarge {...question} />
-          </div>
-        ))}
-      </>
-    </Wrapper>
-  );
 };
 
 export default Questions;
 
 const Wrapper = ({ children }: { children: JSX.Element }) => {
-  return (
-    <div className='bg-darkblue px-6 py-14 xl:p-[56px]'>
-      <div className='grid grid-cols-12 gap-x-6 items-start justify-start'>
-        <div className='hidden lg:flex col-span-3 flex-col gap-y-6'>
-          <TrendingTags />
-          <RelatedQuestions />
+    return (
+        <div className="bg-darkblue px-6 py-14 xl:p-[56px]">
+            <div className="grid grid-cols-12 gap-x-6 items-start justify-start">
+                <div className="hidden lg:flex col-span-3 flex-col gap-y-6">
+                    <TrendingTags />
+                    <RelatedQuestions />
+                </div>
+                <div className="col-span-12 lg:col-span-9 rounded-3xl bg-gray-100 p-9">
+                    <div className="flex flex-col md:flex-row gap-y-6 items-center justify-between text-[32px] text-white mb-16 md:mb-7">
+                        <h1 className="text-[32px] leading-10 text-center xl:text-left m-0">
+                            All Questions
+                        </h1>
+                        <Link
+                            href="/ask-question"
+                            className="no-underline w-full md:w-max cursor-pointer outline-none [border:none] py-[20px] px-[32px] bg-blue rounded-61xl flex flex-row box-border items-center justify-center">
+                            <b className="text-[16px] outline-none tracking-[1.6px] leading-[16px] uppercase text-white text-center font-bold">
+                                Ask A Question
+                            </b>
+                        </Link>
+                    </div>
+                    {children}
+                </div>
+            </div>
         </div>
-        <div className='col-span-12 lg:col-span-9 rounded-3xl bg-gray-100 p-9'>
-          <div className='flex flex-col md:flex-row gap-y-6 items-center justify-between text-[32px] text-white mb-16 md:mb-7'>
-            <h1 className='text-[32px] leading-10 text-center xl:text-left m-0'>
-              All Questions
-            </h1>
-            <Link
-              href='/ask-question'
-              className='no-underline w-full md:w-max cursor-pointer outline-none [border:none] py-[20px] px-[32px] bg-blue rounded-61xl flex flex-row box-border items-center justify-center'>
-              <b className='text-[16px] outline-none tracking-[1.6px] leading-[16px] uppercase text-white text-center font-bold'>
-                Ask A Question
-              </b>
-            </Link>
-          </div>
-          {children}
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
--- src/components/cards/content/index.tsx
+++ src/components/cards/content/index.tsx
@@ -1,150 +1,110 @@
 /* eslint-disable @next/next/no-img-element */
 
-'use client';
-
-import axios from 'axios';
-import { useContractRead, useNetwork, useQuery } from 'wagmi';
-
-import { get_user_by_address_abi } from '@/abi/user';
-import MarkdownRenderer from '@/components/mdx/renderer';
-import AvatarLarge from '@/components/user/avatarLarge';
-import { Address, UserContract, UserMetadata } from '@/types';
-import { create } from 'zustand';
-
-type State = {
-  isCommentActive: boolean;
-  newComment: string;
-};
-
-type Actions = {
-  toggleCommentActive: (isCommentActive: boolean) => void;
-  changeComment: (newComment: string) => void;
-};
-
-const useCountStore = create<State & Actions>((set) => ({
-  isCommentActive: false,
-  newComment: '',
-  toggleCommentActive: (isCommentActive: boolean) =>
-    set((state: State) => ({ ...state, isCommentActive })),
-  changeComment: (newComment: string) =>
-    set((state: State) => ({ ...state, newComment })),
-}));
+import { get_user_by_address_abi } from "@/abi/user";
+import MarkdownRenderer from "@/components/mdx/renderer";
+import AvatarLarge from "@/components/user/avatarLarge";
+import { Address, UserContract, UserMetadata } from "@/types";
+import axios from "axios";
+import {
+    useContractRead,
+    useContractWrite,
+    useNetwork,
+    usePrepareContractWrite,
+    useQuery,
+} from "wagmi";
 
 interface Props {
-  content: string;
-  voteCount: number;
-  comments: any[];
-  authorAddress: Address;
+    content: string;
+    voteCount: number;
+    comments: any[];
+    authorAddress: Address;
 }
 
 const ContentCard = (props: Props) => {
-  const { chain } = useNetwork();
+    const { chain } = useNetwork();
 
-  const { isCommentActive, toggleCommentActive } = useCountStore();
+    const { data, isLoading, isError } = useContractRead({
+        address: process.env.NEXT_PUBLIC_STACK3_ADDRESS as Address,
+        abi: get_user_by_address_abi,
+        functionName: "getUserByAddress",
+        chainId: chain?.id,
+        args: [props?.authorAddress],
+        onError(error: Error) {
+            console.log(error.message);
+        },
+    });
 
-  const {
-    data,
-    isLoading: isUserLoading,
-    isError: isUserError,
-  } = useContractRead({
-    address: process.env.NEXT_PUBLIC_STACK3_ADDRESS as Address,
-    abi: get_user_by_address_abi,
-    functionName: 'getUserByAddress',
-    chainId: chain?.id,
-    args: [props?.authorAddress],
-    onError(error: Error) {
-      console.log(error.message);
-    },
-  });
+    // const { config: upvote_config } = usePrepareContractWrite({
+    //     address: process.env.NEXT_PUBLIC_STACK3_ADDRESS as Address,
+    //     abi: post_question_abi,
+    //     functionName: "postQuestion",
+    //     args: [[1, 2, 3], url, process.env.NEXT_PUBLIC_HASH_SECRET],
+    // });
 
-  const author = data as UserContract;
+    // const { write: post_question } = useContractWrite({
+    //     ...post_question_config,
+    //     onError(error) {
+    //         console.log(error);
+    //     },
+    //     async onSuccess(data) {
+    //         await data.wait();
+    //         alert("Question Posted Successfully!");
+    //         changeUrl("");
+    //     },
+    // });
 
-  const {
-    data: profile,
-    isError: isProfileError,
-    isLoading: isProfileLoading,
-  } = useQuery(['user-profile', props?.authorAddress], () =>
-    axios.get(author?.uri)
-  );
+    console.log({ Data: data });
 
-  const user = profile?.data as UserMetadata;
+    const author = data as UserContract;
 
-  if (isUserLoading || isProfileLoading) {
-    return (
-      <Wrapper content={props.content} voteCount={props.voteCount}>
-        <div>Loading...</div>
-      </Wrapper>
+    console.log(author);
+
+    const {
+        data: profile,
+        isError: isProfileError,
+        isLoading: isProfileLoading,
+    } = useQuery(["user-profile", props?.authorAddress], () =>
+        axios.get(author?.uri)
     );
-  }
 
-  if (isUserError || isProfileError) {
+    console.log(profile);
+
+    const user = profile?.data as UserMetadata;
+
+    console.log(user);
+
     return (
-      <Wrapper content={props.content} voteCount={props.voteCount}>
-        <div>Something went wrong</div>
-      </Wrapper>
-    );
-  }
-
-  if (!author || !user) {
-    return (
-      <Wrapper content={props.content} voteCount={props.voteCount}>
-        <div>No data found redarding author.</div>
-      </Wrapper>
-    );
-  }
-
-  return (
-    <Wrapper content={props.content} voteCount={props.voteCount}>
-      <div className='flex items-center justify-between mt-10 w-full'>
-        <button className='border-none py-3 px-5 rounded-md cursor-pointer font-semibold text-dark-blue'>
-          Add comment
-        </button>
-        <AvatarLarge
-          image={user?.profile}
-          name={user?.name}
-          id={author?.id?.toString()}
-        />
-      </div>
-    </Wrapper>
-  );
-};
-
-export default ContentCard;
-
-const Wrapper = ({
-  content,
-  voteCount,
-  children,
-}: {
-  children: JSX.Element;
-  content: string;
-  voteCount: number;
-}) => {
-  return (
-    <div className='rounded-xl bg-gray-500 px-4 py-6 xl:p-8 flex flex-row items-start gap-x-4'>
-      <div className='flex flex-col items-center gap-y-2 justify-center text-silver-100'>
-        <button className='border-none bg-transparent w-[max-content] h-[max-content] flex m-0 p-0 cursor-pointer'>
-          <img
-            className='relative w-8 h-[19px]'
-            alt='Upvote'
-            src='/vector.svg'
-          />
-        </button>
-        <div className='relative font-medium'>{voteCount}</div>
-        <button className='border-none bg-transparent w-[max-content] h-[max-content] flex m-0 p-0 cursor-pointer'>
-          <img
-            className='relative w-8 h-[19px]'
-            alt='Downvote'
-            src='/vector1.svg'
-          />
-        </button>
-      </div>
-      <div>
-        <div>
-          <MarkdownRenderer markdown={content} />
+        <div className="rounded-xl bg-gray-500 px-4 py-6 xl:p-8 flex flex-row items-start gap-x-4">
+            <div className="flex flex-col items-center gap-y-2 justify-center text-silver-100">
+                <button className="border-none bg-transparent w-[max-content] h-[max-content] flex m-0 p-0 cursor-pointer">
+                    <img
+                        className="relative w-8 h-[19px]"
+                        alt="Upvote"
+                        src="/vector.svg"
+                    />
+                </button>
+                <div className="relative font-medium">{props.voteCount}</div>
+                <button className="border-none bg-transparent w-[max-content] h-[max-content] flex m-0 p-0 cursor-pointer">
+                    <img
+                        className="relative w-8 h-[19px]"
+                        alt="Downvote"
+                        src="/vector1.svg"
+                    />
+                </button>
+            </div>
+            <div>
+                <div>
+                    <MarkdownRenderer markdown={props.content} />
+                </div>
+                <div className="flex items-center justify-between mt-10 w-full">
+                    <button>Add comment</button>
+                    <AvatarLarge
+                        image={user?.profile}
+                        name={user?.name}
+                        id={author?.id?.toString()}
+                    />
+                </div>
+            </div>
         </div>
-        {children}
-      </div>
-    </div>
-  );
+    );
 };
