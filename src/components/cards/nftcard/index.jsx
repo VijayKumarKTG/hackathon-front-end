767968ee233042d3e859b5e64d8bf177a564bb6b
AyushIyankan <ayushiyankan007@gmail.com>
2023-05-21 22:19:33 +0530

    completed achievements skeleton

--- src/components/cards/nftcard/index.jsx
+++ src/components/cards/nftcard/index.jsx
@@ -1,16 +1,30 @@
-const NFTCard = () => {
-    return (
+import Skeleton from "react-loading-skeleton";
+
+import "react-loading-skeleton/dist/skeleton.css";
+
+const NFTCard = ({ isFetching }) => {
+    return isFetching ? (
+        <div className="mb-2 h-[350px] w-[286px]  lg:h-[377px] lg:w-[300px] rounded-xl">
+            <Skeleton
+                baseColor="#22294d"
+                highlightColor="#313a67"
+                width="100%"
+                height="100%"
+                borderRadius={20}
+            />
+        </div>
+    ) : (
         <div
             className="p-2 bg-blue bg-gradient-to-t from-blue-500 to-[#011163] rounded-xl mb-2"
             style={{
                 background: "linear-gradient(180deg, #0328EE 0%, #011163 100%)",
             }}>
-            <div className="flex flex-col items-center justify-center max-h-[361px] max-w-[298px] rounded-xl">
-                <div className="flex max-h-[260px] w-auto object-cover rounded-tr-xl rounded-tl-xl">
+            <div className="flex flex-col items-center justify-center max-h-[331px] max-w-[270px]  lg:max-h-[361px] lg:max-w-[298px] rounded-xl">
+                <div className="flex w-[300px] max-h-[230px] lg:max-h-[260px] lg:w-auto object-cover rounded-tr-xl rounded-tl-xl items-center">
                     <img
                         src="nft-sample.png"
                         alt="NFT Image"
-                        className="rounded-tr-xl rounded-tl-xl"
+                        className="mx-auto rounded-tr-xl rounded-tl-xl object-cover w-[267px] max-h-[225px] lg:max-h-[260px] lg:w-auto"
                     />
                 </div>
                 <div className="flex flex-col items-center justify-center p-[19px] w-full bg-[#030C30] rounded-br-xl rounded-bl-xl">
