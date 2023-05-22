33de69b4388636c959f56e7c7616a0c45f0098ad
AyushIyankan <ayushiyankan007@gmail.com>
2023-05-23 01:13:16 +0530

    build about us, responsive pending

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,302 @@
 const About = () => {
-  return <div>About Us</div>;
+    return (
+        <div className="flex flex-col items-center justify-center mt-24 text-[4.13rem] text-white">
+            <div className="flex flex-col items-center justify-center w-[37.88rem] h-[12.38rem] text-center">
+                <b className="tracking-[-1px] leading-[5rem] text-white">
+                    About Web3dApp
+                </b>
+                <div className="text-[1.13rem] leading-[2rem] flex items-center justify-center w-[37.88rem] text-white">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Feugiat nulla suspendisse tortor aenean dis placerat.
+                    Scelerisque imperdiet vitae dolor non aliquam. Malesuada.
+                </div>
+            </div>
+            <div className="h-[38.31rem] flex flex-col items-center justify-start gap-[1.81rem] text-[2.63rem] mt-[112px]">
+                <div className="flex flex-row items-center justify-between w-[74.88rem] h-[4rem]">
+                    <div className="leading-[3.44rem]">
+                        <b>What drives Web3dApp?</b>
+                    </div>
+                    <div className="text-[1.13rem] leading-[2rem] w-[31rem]">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Feugiat nulla suspendisse tortor aene.
+                    </div>
+                </div>
+                <div className="grid grid-rows-2 grid-cols-2 gap-7 w-[74.88rem] h-[32.5rem] text-[1.13rem]">
+                    <div className="rounded-21xl bg-dark-blue overflow-hidden flex flex-row py-[3.63rem] px-[2rem] items-start justify-start gap-[1.5rem]">
+                        <div className="flex flex-row items-center justify-center rounded-xl bg-blue w-[4.25rem] h-[4.25rem] overflow-hidden shrink-0">
+                            <div className="flex flex-row items-center justify-center">
+                                <img
+                                    className="flex flex-col w-[2.94rem] h-[2.19rem] overflow-hidden shrink-0"
+                                    alt=""
+                                    src="/60e4a1e73807563947e7fb99-iconsectionabout1cryptotemplatesvg.svg"
+                                />
+                            </div>
+                        </div>
+                        <div className=" w-[26.81rem] h-[8.13rem]">
+                            <b className="tracking-[0.9px] leading-[1.62rem] uppercase">
+                                Ask and answer questions
+                            </b>
+                            <div className="leading-[2rem] flex items-center w-[25.13rem]">
+                                Lorem ipsum dolor sit amet, consectetur
+                                adipiscing elit. Et nibh urna in proin dui purus
+                                bibendum cras. Morbi cursus nunc.
+                            </div>
+                        </div>
+                    </div>
+                    <div className="rounded-21xl bg-dark-blue overflow-hidden flex flex-row py-[3.63rem] px-[2rem] items-start justify-start gap-[1.5rem]">
+                        <div className="flex flex-row items-center justify-center rounded-xl bg-blue w-[4.25rem] h-[4.25rem] overflow-hidden shrink-0">
+                            <div className="flex flex-row items-center justify-center">
+                                <img
+                                    className="overflow-hidden shrink-0"
+                                    alt=""
+                                    src="/60e4a1e7380756fecde7fb9c-iconsectionabout2cryptotemplatesvg.svg"
+                                />
+                            </div>
+                        </div>
+                        <div className="w-[26.81rem] h-[8.13rem]">
+                            <b className="tracking-[0.9px] leading-[1.62rem] uppercase">
+                                see stats and upvotes
+                            </b>
+                            <div className="leading-[2rem] flex items-center w-[25.13rem]">
+                                Lorem ipsum dolor sit amet, consectetur
+                                adipiscing elit. Et nibh urna in proin dui purus
+                                bibendum cras. Morbi cursus nunc.
+                            </div>
+                        </div>
+                    </div>
+                    <div className="rounded-21xl bg-dark-blue overflow-hidden flex flex-row py-[3.63rem] px-[2rem] items-start justify-start gap-[1.5rem]">
+                        <div className="flex flex-row items-center justify-center rounded-xl bg-blue w-[4.25rem] h-[4.25rem] overflow-hidden shrink-0">
+                            <div className="flex flex-row items-center justify-center">
+                                <img
+                                    className="w-[2.88rem] h-[2.88rem] overflow-hidden shrink-0"
+                                    alt=""
+                                    src="/60e4a1e73807565f69e7fb9b-iconsectionabout3cryptotemplatesvg.svg"
+                                />
+                            </div>
+                        </div>
+                        <div className="w-[26.81rem] h-[8.13rem]">
+                            <b className="tracking-[0.9px] leading-[1.62rem] uppercase">
+                                get rewards
+                            </b>
+                            <div className="leading-[2rem] flex items-center w-[25.13rem]">
+                                Lorem ipsum dolor sit amet, consectetur
+                                adipiscing elit. Et nibh urna in proin dui purus
+                                bibendum cras. Morbi cursus nunc.
+                            </div>
+                        </div>
+                    </div>
+                    <div className="rounded-21xl bg-dark-blue overflow-hidden flex flex-row py-[3.63rem] px-[2rem] items-start justify-start gap-[1.5rem]">
+                        <div className="flex flex-row items-center justify-center rounded-xl bg-blue w-[4.25rem] h-[4.25rem] overflow-hidden shrink-0">
+                            <div className="flex flex-row items-center justify-center">
+                                <img
+                                    className="w-[2.81rem] h-[2.81rem] overflow-hidden shrink-0"
+                                    alt=""
+                                    src="/60e4a1e73807561d83e7fbc5-iconaboutdrive3cryptotemplatesvg.svg"
+                                />
+                            </div>
+                        </div>
+                        <div className="w-[26.81rem] h-[8.13rem]">
+                            <b className="tracking-[0.9px] leading-[1.62rem] uppercase">
+                                all decentralised
+                            </b>
+                            <div className="leading-[2rem] flex items-center w-[25.13rem]">
+                                Lorem ipsum dolor sit amet, consectetur
+                                adipiscing elit. Et nibh urna in proin dui purus
+                                bibendum cras. Morbi cursus nunc.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="flex flex-col items-center justify-center mt-[112px] w-[74.88rem] h-[41.13rem] text-[1.25rem]">
+                <div className="flex flex-row items-center justify-between w-[74.88rem] h-[4rem] text-[2.63rem]">
+                    <b className="leading-[3.44rem]">Our Team</b>
+                    <div className="text-[1.13rem] leading-[2rem] flex items-center w-[31rem]">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Parturient lorem purus justo, ultricies.
+                    </div>
+                </div>
+                <div className="flex flex-row gap-[38px] items-center justify-center mt-[66px]">
+                    <div className="rounded-[32px] bg-dark-blue w-[23.38rem] overflow-hidden flex flex-col p-[1.5rem] box-border items-start justify-start gap-[2rem]">
+                        <img
+                            className="rounded-[32px] w-[20.44rem] h-[25rem] object-cover"
+                            alt=""
+                            src="/vijay.jpeg"
+                        />
+                        <div className="w-[20.44rem] flex flex-col py-[0rem] pr-[3.43rem] pl-[0rem] box-border items-start justify-center">
+                            <div className="flex flex-col items-start justify-start gap-[0.75rem]">
+                                <b className="tracking-[0.8px] leading-[1.13rem] uppercase">
+                                    VIJAY KUMAr
+                                </b>
+                                <div className="text-[1rem] tracking-[0.8px] leading-[1.13rem] uppercase">
+                                    core team
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="rounded-[32px] bg-dark-blue w-[23.38rem] overflow-hidden flex flex-col p-[1.5rem] box-border items-start justify-start gap-[2rem]">
+                        <img
+                            className="rounded-[32px] w-[20.44rem] h-[25rem] object-cover"
+                            alt=""
+                            src="/ayush.jpeg"
+                        />
+                        <div className="flex flex-col items-start justify-start gap-[0.75rem]">
+                            <b className="tracking-[0.8px] leading-[1.13rem] uppercase">
+                                Ayush i
+                            </b>
+                            <div className="text-[1rem] tracking-[0.8px] leading-[1.13rem] uppercase">
+                                core team
+                            </div>
+                        </div>
+                    </div>
+                    <div className="rounded-[32px] bg-dark-blue w-[23.38rem] overflow-hidden flex flex-col p-[1.5rem] box-border items-start justify-start gap-[2rem]">
+                        <img
+                            className="rounded-[32px] w-[20.44rem] h-[25rem] object-cover"
+                            alt=""
+                            src="/ayan.jpeg"
+                        />
+                        <div className="flex flex-col items-start justify-start gap-[0.75rem]">
+                            <b className="tracking-[0.8px] leading-[1.13rem] uppercase">
+                                Ayan Maiti
+                            </b>
+                            <div className="text-[1rem] tracking-[0.8px] leading-[1.13rem] uppercase">
+                                core team
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="flex flex-col w-[67.81rem] h-[26.5rem] text-center text-[4rem] mt-[112px]">
+                <div className="flex flex-col items-center justify-start gap-[1rem] text-[2.63rem]">
+                    <b className="leading-[3.44rem]">
+                        You made it possible for us!
+                    </b>
+                    <div className="text-[1.13rem] leading-[2rem] flex items-center justify-center w-[31rem]">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Parturient lorem purus justo, ultricies.
+                    </div>
+                </div>
+                <div className="flex flex-row items-center justify-between mt-[67px]">
+                    <div className="flex flex-row items-center justify-center rounded-xl bg-blue box-border w-[31.81rem] h-[13.81rem] overflow-hidden border-[4px] border-solid border-white gap-[30px]">
+                        <img
+                            className="flex flex-row top-[0rem] left-[0rem] w-[8rem] h-[8rem]"
+                            alt=""
+                            src="/chainlink-white.png"
+                        />
+                        <div className="flex flex-row">
+                            <div className="flex flex-row leading-[3.44rem]">
+                                Chainlink
+                            </div>
+                        </div>
+                    </div>
+                    <div className="flex flex-row items-center justify-center rounded-xl bg-blue box-border w-[31.81rem] h-[13.81rem] overflow-hidden border-[4px] border-solid border-white gap-3">
+                        <img
+                            className="flex flex-row top-[0rem] left-[0rem] w-[8rem] h-[8rem]"
+                            alt=""
+                            src="/polygon-white.png"
+                        />
+                        <div className="flex flex-row">
+                            <div className="flex flex-row leading-[3.44rem]">
+                                Polygon
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="flex flex-col items-center justify-center w-[67.81rem] text-center text-[2.63rem] mt-[112px] mb-[310px]">
+                <div className="flex flex-col top-[0rem] left-[20.69rem] w-[31rem] h-[8.5rem]">
+                    <div className="flex flex-col top-[0rem] left-[0rem] w-[31rem] h-[8.5rem]">
+                        <b className="flex flex-col top-[0rem] left-[8.53rem] leading-[3.44rem]">
+                            Tech Stack
+                        </b>
+                        <div className="flex flex-col top-[4.5rem] left-[0rem] text-[1.13rem] leading-[2rem] items-center justify-center w-[31rem]">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing
+                            elit. Parturient lorem purus justo, ultricies.
+                        </div>
+                    </div>
+                </div>
+                <div className="grid grid-cols-4 grid-rows-3 gap-[67px] mt-14">
+                    <div className="flex flex-col items-center justify-center rounded-xl bg-blue w-[13.81rem] h-[13.81rem] overflow-hidden">
+                        <img
+                            className="h-[128px] rounded-xl w-[13.81rem] h-[13.81rem] overflow-hidden object-contain"
+                            alt=""
+                            src="/solidity-white.png"
+                        />
+                    </div>
+                    <div className="flex flex-col items-center justify-center rounded-xl bg-blue w-[13.81rem] h-[13.81rem] overflow-hidden">
+                        <img
+                            className="h-[128px] rounded-xl w-[13.81rem] h-[13.81rem] overflow-hidden object-contain"
+                            alt=""
+                            src="/hardhat-white.png"
+                        />
+                    </div>
+                    <div className="flex flex-col items-center justify-center rounded-xl bg-blue w-[13.81rem] h-[13.81rem] overflow-hidden">
+                        <img
+                            className="h-[128px] rounded-xl w-[13.81rem] h-[13.81rem] overflow-hidden object-contain"
+                            alt=""
+                            src="/chai-white.png"
+                        />
+                    </div>
+                    <div className="flex flex-col items-center justify-center rounded-xl bg-blue w-[13.81rem] h-[13.81rem] overflow-hidden">
+                        <img
+                            className="h-[128px] rounded-xl w-[13.81rem] h-[13.81rem] overflow-hidden object-contain"
+                            alt=""
+                            src="/moccha-white.png"
+                        />
+                    </div>
+                    <div className="flex flex-col items-center justify-center rounded-xl bg-blue w-[13.81rem] h-[13.81rem] overflow-hidden">
+                        <img
+                            className="h-[128px] rounded-xl w-[13.81rem] h-[13.81rem] overflow-hidden object-contain"
+                            alt=""
+                            src="/next-white.png"
+                        />
+                    </div>
+                    <div className="flex flex-col items-center justify-center rounded-xl bg-blue w-[13.81rem] h-[13.81rem] overflow-hidden">
+                        <img
+                            className="h-[128px] rounded-xl w-[13.81rem] h-[13.81rem] overflow-hidden object-contain"
+                            alt=""
+                            src="/ts-white.png"
+                        />
+                    </div>
+                    <div className="flex flex-col items-center justify-center rounded-xl bg-blue w-[13.81rem] h-[13.81rem] overflow-hidden">
+                        <img
+                            className="h-[128px] rounded-xl w-[13.81rem] h-[13.81rem] overflow-hidden object-contain"
+                            alt=""
+                            src="/tailwind-white.png"
+                        />
+                    </div>
+                    <div className="flex flex-col items-center justify-center rounded-xl bg-blue w-[13.81rem] h-[13.81rem] overflow-hidden">
+                        <img
+                            className="h-[128px] rounded-xl w-[13.81rem] h-[13.81rem] overflow-hidden object-contain mix-blend-lighten"
+                            alt=""
+                            src="/wagmi.png"
+                        />
+                    </div>
+                    <div className="flex flex-col items-center justify-center rounded-xl bg-blue w-[13.81rem] h-[13.81rem] overflow-hidden">
+                        <img
+                            className="h-[128px] rounded-xl w-[13.81rem] h-[13.81rem] overflow-hidden object-contain"
+                            alt=""
+                            src="/alchemy-white.png"
+                        />
+                    </div>
+                    <div className="flex flex-col items-center justify-center rounded-xl bg-blue w-[13.81rem] h-[13.81rem] overflow-hidden">
+                        <img
+                            className="h-[128px] rounded-xl w-[13.81rem] h-[13.81rem] overflow-hidden object-contain"
+                            alt=""
+                            src="/figma-white.png"
+                        />
+                    </div>
+                    <div className="flex flex-col items-center justify-center rounded-xl bg-blue w-[13.81rem] h-[13.81rem] overflow-hidden">
+                        <img
+                            className="h-[128px] rounded-xl w-[13.81rem] h-[13.81rem] overflow-hidden object-contain"
+                            alt=""
+                            src="/notion-white.png"
+                        />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default About;
