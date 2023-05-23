4a98322795a2405e4bab3acf86fb878916ea8f19
AyushIyankan <ayushiyankan007@gmail.com>
2023-05-23 11:05:46 +0530

    tags input is working perfectly

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,7 @@
 const About = () => {
     return (
         <div className="flex flex-col items-center justify-center mt-24 text-[4.13rem] text-white">
-            <div className="flex flex-col items-center justify-center w-[37.88rem] h-[12.38rem] text-center">
+            <div className="flex flex-col items-center justify-center w-auto h-auto text-center">
                 <b className="tracking-[-1px] leading-[5rem] text-white">
                     About Web3dApp
                 </b>
@@ -11,7 +11,7 @@ const About = () => {
                     Scelerisque imperdiet vitae dolor non aliquam. Malesuada.
                 </div>
             </div>
-            <div className="h-[38.31rem] flex flex-col items-center justify-start gap-[1.81rem] text-[2.63rem] mt-[112px]">
+            <div className=" w-auto h-auto flex flex-col items-center justify-start gap-[1.81rem] text-[2.63rem] mt-[112px]">
                 <div className="flex flex-row items-center justify-between w-[74.88rem] h-[4rem]">
                     <div className="leading-[3.44rem]">
                         <b>What drives Web3dApp?</b>
@@ -21,9 +21,9 @@ const About = () => {
                         Feugiat nulla suspendisse tortor aene.
                     </div>
                 </div>
-                <div className="grid grid-rows-2 grid-cols-2 gap-7 w-[74.88rem] h-[32.5rem] text-[1.13rem]">
+                <div className="grid grid-rows-2 grid-cols-2 gap-7 max-w-[74.88rem] max-h-[32.5rem] text-[1.13rem]">
                     <div className="rounded-21xl bg-dark-blue overflow-hidden flex flex-row py-[3.63rem] px-[2rem] items-start justify-start gap-[1.5rem]">
-                        <div className="flex flex-row items-center justify-center rounded-xl bg-blue w-[4.25rem] h-[4.25rem] overflow-hidden shrink-0">
+                        <div className="flex flex-row items-center justify-center rounded-xl bg-blue w-5overflow-hidden shrink-0">
                             <div className="flex flex-row items-center justify-center">
                                 <img
                                     className="flex flex-col w-[2.94rem] h-[2.19rem] overflow-hidden shrink-0"
@@ -32,7 +32,7 @@ const About = () => {
                                 />
                             </div>
                         </div>
-                        <div className=" w-[26.81rem] h-[8.13rem]">
+                        <div className="">
                             <b className="tracking-[0.9px] leading-[1.62rem] uppercase">
                                 Ask and answer questions
                             </b>
@@ -53,7 +53,7 @@ const About = () => {
                                 />
                             </div>
                         </div>
-                        <div className="w-[26.81rem] h-[8.13rem]">
+                        <div className="">
                             <b className="tracking-[0.9px] leading-[1.62rem] uppercase">
                                 see stats and upvotes
                             </b>
@@ -108,7 +108,7 @@ const About = () => {
                     </div>
                 </div>
             </div>
-            <div className="flex flex-col items-center justify-center mt-[112px] w-[74.88rem] h-[41.13rem] text-[1.25rem]">
+            <div className="flex flex-col items-center justify-center mt-[112px] w-auto h-auto  text-[1.25rem]">
                 <div className="flex flex-row items-center justify-between w-[74.88rem] h-[4rem] text-[2.63rem]">
                     <b className="leading-[3.44rem]">Our Team</b>
                     <div className="text-[1.13rem] leading-[2rem] flex items-center w-[31rem]">
@@ -117,7 +117,7 @@ const About = () => {
                     </div>
                 </div>
                 <div className="flex flex-row gap-[38px] items-center justify-center mt-[66px]">
-                    <div className="rounded-[32px] bg-dark-blue w-[23.38rem] overflow-hidden flex flex-col p-[1.5rem] box-border items-start justify-start gap-[2rem]">
+                    <div className="rounded-[32px] bg-dark-blue w-auto h-auto overflow-hidden flex flex-col p-[1.5rem] box-border items-start justify-start gap-[2rem]">
                         <img
                             className="rounded-[32px] w-[20.44rem] h-[25rem] object-cover"
                             alt=""
@@ -134,7 +134,7 @@ const About = () => {
                             </div>
                         </div>
                     </div>
-                    <div className="rounded-[32px] bg-dark-blue w-[23.38rem] overflow-hidden flex flex-col p-[1.5rem] box-border items-start justify-start gap-[2rem]">
+                    <div className="rounded-[32px] bg-dark-blue w-auto h-auto overflow-hidden flex flex-col p-[1.5rem] box-border items-start justify-start gap-[2rem]">
                         <img
                             className="rounded-[32px] w-[20.44rem] h-[25rem] object-cover"
                             alt=""
@@ -166,7 +166,7 @@ const About = () => {
                     </div>
                 </div>
             </div>
-            <div className="flex flex-col w-[67.81rem] h-[26.5rem] text-center text-[4rem] mt-[112px]">
+            <div className="flex flex-col w-auto h-auto  text-center text-[4rem] mt-[112px]">
                 <div className="flex flex-col items-center justify-start gap-[1rem] text-[2.63rem]">
                     <b className="leading-[3.44rem]">
                         You made it possible for us!
@@ -203,10 +203,10 @@ const About = () => {
                     </div>
                 </div>
             </div>
-            <div className="flex flex-col items-center justify-center w-[67.81rem] text-center text-[2.63rem] mt-[112px] mb-[310px]">
-                <div className="flex flex-col top-[0rem] left-[20.69rem] w-[31rem] h-[8.5rem]">
-                    <div className="flex flex-col top-[0rem] left-[0rem] w-[31rem] h-[8.5rem]">
-                        <b className="flex flex-col top-[0rem] left-[8.53rem] leading-[3.44rem]">
+            <div className="flex flex-col items-center justify-center w-auto h-auto text-center text-[2.63rem] mt-[112px] mb-[310px]">
+                <div className="flex flex-col w-auto h-auto">
+                    <div className="flex flex-col top-[0rem] w-auto h-auto">
+                        <b className="flex flex-col  w-auto h-auto leading-[3.44rem]">
                             Tech Stack
                         </b>
                         <div className="flex flex-col top-[4.5rem] left-[0rem] text-[1.13rem] leading-[2rem] items-center justify-center w-[31rem]">
@@ -218,49 +218,49 @@ const About = () => {
                 <div className="grid grid-cols-4 grid-rows-3 gap-[67px] mt-14">
                     <div className="flex flex-col items-center justify-center rounded-xl bg-blue w-[13.81rem] h-[13.81rem] overflow-hidden">
                         <img
-                            className="h-[128px] rounded-xl w-[13.81rem] h-[13.81rem] overflow-hidden object-contain"
+                            className="h-[128px] rounded-xl w-[13.81rem] h-[13.81rem] overflow-hidden object-contain py-[46px]"
                             alt=""
                             src="/solidity-white.png"
                         />
                     </div>
                     <div className="flex flex-col items-center justify-center rounded-xl bg-blue w-[13.81rem] h-[13.81rem] overflow-hidden">
                         <img
-                            className="h-[128px] rounded-xl w-[13.81rem] h-[13.81rem] overflow-hidden object-contain"
+                            className="h-[128px] rounded-xl w-[13.81rem] h-[13.81rem] overflow-hidden object-contain py-[46px]"
                             alt=""
                             src="/hardhat-white.png"
                         />
                     </div>
                     <div className="flex flex-col items-center justify-center rounded-xl bg-blue w-[13.81rem] h-[13.81rem] overflow-hidden">
                         <img
-                            className="h-[128px] rounded-xl w-[13.81rem] h-[13.81rem] overflow-hidden object-contain"
+                            className="h-[128px] rounded-xl w-[13.81rem] h-[13.81rem] overflow-hidden object-contain py-[46px]"
                             alt=""
                             src="/chai-white.png"
                         />
                     </div>
                     <div className="flex flex-col items-center justify-center rounded-xl bg-blue w-[13.81rem] h-[13.81rem] overflow-hidden">
                         <img
-                            className="h-[128px] rounded-xl w-[13.81rem] h-[13.81rem] overflow-hidden object-contain"
+                            className="h-[128px] rounded-xl w-[13.81rem] h-[13.81rem] overflow-hidden object-contain py-[46px]"
                             alt=""
                             src="/moccha-white.png"
                         />
                     </div>
                     <div className="flex flex-col items-center justify-center rounded-xl bg-blue w-[13.81rem] h-[13.81rem] overflow-hidden">
                         <img
-                            className="h-[128px] rounded-xl w-[13.81rem] h-[13.81rem] overflow-hidden object-contain"
+                            className="h-[128px] rounded-xl w-[13.81rem] h-[13.81rem] overflow-hidden object-contain py-[46px]"
                             alt=""
                             src="/next-white.png"
                         />
                     </div>
                     <div className="flex flex-col items-center justify-center rounded-xl bg-blue w-[13.81rem] h-[13.81rem] overflow-hidden">
                         <img
-                            className="h-[128px] rounded-xl w-[13.81rem] h-[13.81rem] overflow-hidden object-contain"
+                            className="h-[128px] rounded-xl w-[13.81rem] h-[13.81rem] overflow-hidden object-contain py-[46px]"
                             alt=""
                             src="/ts-white.png"
                         />
                     </div>
                     <div className="flex flex-col items-center justify-center rounded-xl bg-blue w-[13.81rem] h-[13.81rem] overflow-hidden">
                         <img
-                            className="h-[128px] rounded-xl w-[13.81rem] h-[13.81rem] overflow-hidden object-contain"
+                            className="h-[128px] rounded-xl w-[13.81rem] h-[13.81rem] overflow-hidden object-contain py-[46px]"
                             alt=""
                             src="/tailwind-white.png"
                         />
@@ -274,21 +274,21 @@ const About = () => {
                     </div>
                     <div className="flex flex-col items-center justify-center rounded-xl bg-blue w-[13.81rem] h-[13.81rem] overflow-hidden">
                         <img
-                            className="h-[128px] rounded-xl w-[13.81rem] h-[13.81rem] overflow-hidden object-contain"
+                            className="h-[128px] rounded-xl w-[13.81rem] h-[13.81rem] overflow-hidden object-contain py-[46px]"
                             alt=""
                             src="/alchemy-white.png"
                         />
                     </div>
                     <div className="flex flex-col items-center justify-center rounded-xl bg-blue w-[13.81rem] h-[13.81rem] overflow-hidden">
                         <img
-                            className="h-[128px] rounded-xl w-[13.81rem] h-[13.81rem] overflow-hidden object-contain"
+                            className="h-[128px] rounded-xl w-[13.81rem] h-[13.81rem] overflow-hidden object-contain py-[46px]"
                             alt=""
                             src="/figma-white.png"
                         />
                     </div>
                     <div className="flex flex-col items-center justify-center rounded-xl bg-blue w-[13.81rem] h-[13.81rem] overflow-hidden">
                         <img
-                            className="h-[128px] rounded-xl w-[13.81rem] h-[13.81rem] overflow-hidden object-contain"
+                            className="h-[128px] rounded-xl w-[13.81rem] h-[13.81rem] overflow-hidden object-contain py-[46px]"
                             alt=""
                             src="/notion-white.png"
                         />
