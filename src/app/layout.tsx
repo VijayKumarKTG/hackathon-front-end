b07486791ca46fd517bc95667238217b1ea2ee95
AyushIyankan <ayushiyankan007@gmail.com>
2023-06-01 22:41:35 +0530

    Add WalletConnect Button in the connect wallet page, add wallet errors to modal

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,127 +1,129 @@
-'use client';
-import Navigation from '@/components/navigation';
-import { DM_Sans } from 'next/font/google';
-import { WagmiConfig, configureChains, createClient, useNetwork } from 'wagmi';
-import { polygonMumbai } from 'wagmi/chains';
-import { alchemyProvider } from 'wagmi/providers/alchemy';
-import { useEffect, useState } from 'react';
+"use client";
+import Navigation from "@/components/navigation";
+import { DM_Sans } from "next/font/google";
+import { WagmiConfig, configureChains, createClient, useNetwork } from "wagmi";
+import { polygonMumbai } from "wagmi/chains";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { useEffect, useState } from "react";
 // import { publicProvider } from 'wagmi/providers/public';
-import { MetaMaskConnector } from 'wagmi/connectors/metaMask';
-import { CoinbaseWalletConnector } from 'wagmi/connectors/coinbaseWallet';
+import { MetaMaskConnector } from "wagmi/connectors/metaMask";
+import { CoinbaseWalletConnector } from "wagmi/connectors/coinbaseWallet";
+import { WalletConnectConnector } from "wagmi/connectors/walletConnect";
+
 // import { jsonRpcProvider } from 'wagmi/providers/jsonRpc';
 // import { WalletConnectConnector } from 'wagmi/connectors/walletConnect';
 
-import './globals.css';
-import Footer from '@/components/footer';
-import LoadingModal from '@/components/modals/loader';
-import ErrorModal from '@/components/modals/error';
-import SuccessModal from '@/components/modals/success';
+import "./globals.css";
+import Footer from "@/components/footer";
+import LoadingModal from "@/components/modals/loader";
+import ErrorModal from "@/components/modals/error";
+import SuccessModal from "@/components/modals/success";
 
 // -------------- WAGMI CONFIG STARTS ----------------
 
 const { chains, provider, webSocketProvider } = configureChains(
-  [polygonMumbai],
-  [
-    // jsonRpcProvider({
-    //   rpc: () => ({
-    //     http: `http://127.0.0.1:8545/`,
-    //   }),
-    // }),
-    alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY! }),
-    // publicProvider(),
-  ]
+    [polygonMumbai],
+    [
+        // jsonRpcProvider({
+        //   rpc: () => ({
+        //     http: `http://127.0.0.1:8545/`,
+        //   }),
+        // }),
+        alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY! }),
+        // publicProvider(),
+    ]
 );
 
 const client = createClient({
-  autoConnect: true,
-  connectors: [
-    new MetaMaskConnector({ chains }),
-    new CoinbaseWalletConnector({
-      chains,
-      options: {
-        appName: 'Hackathon',
-        jsonRpcUrl:
-          'https://eth-mainnet.g.alchemy.com/v2/5j7hyZzXJirxp1CV2MjzPNB5YM8y3oA8',
-      },
-    }),
-    // new WalletConnectConnector({
-    //   chains,
-    //   options: {
-    //     projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!,
-    //   },
-    // }),
-  ],
-  provider,
-  webSocketProvider,
+    autoConnect: true,
+    connectors: [
+        new MetaMaskConnector({ chains }),
+        new CoinbaseWalletConnector({
+            chains,
+            options: {
+                appName: "Hackathon",
+                jsonRpcUrl:
+                    "https://eth-mainnet.g.alchemy.com/v2/5j7hyZzXJirxp1CV2MjzPNB5YM8y3oA8",
+            },
+        }),
+        new WalletConnectConnector({
+            chains,
+            options: {
+                projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!,
+            },
+        }),
+    ],
+    provider,
+    webSocketProvider,
 });
 
 // -------------- WAGMI CONFIG ENDS ----------------
 
-const dm_sans = DM_Sans({ weight: ['400', '500', '700'], subsets: ['latin'] });
+const dm_sans = DM_Sans({ weight: ["400", "500", "700"], subsets: ["latin"] });
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }) {
-  const [errorTitle, setErrorTitle] = useState<string>('');
-  const [errorMessage, setErrorMessage] = useState<string>('');
-  const [successTitle, setSuccessTitle] = useState<string>('');
-  const [successMessage, setSuccessMessage] = useState<string>('');
-  const [loadingTitle, setLoadingTitle] = useState<string>('');
-  const [loadingMessage, setLoadingMessage] = useState<string>('');
+    const [errorTitle, setErrorTitle] = useState<string>("");
+    const [errorMessage, setErrorMessage] = useState<string>("");
+    const [successTitle, setSuccessTitle] = useState<string>("");
+    const [successMessage, setSuccessMessage] = useState<string>("");
+    const [loadingTitle, setLoadingTitle] = useState<string>("");
+    const [loadingMessage, setLoadingMessage] = useState<string>("");
 
-  const { chain } = useNetwork();
+    const { chain } = useNetwork();
 
-  useEffect(() => {
-    if (chain && chain!?.id != 80001) {
-      setErrorTitle('Please connect to polygon mumbai testnet');
-      setErrorMessage('Other networks are not supported currently.');
-      setSuccessTitle('');
-      setSuccessMessage('');
-      setLoadingTitle('');
-      setLoadingMessage('');
-    } else if (chain && chain!?.id == 80001) {
-      setErrorTitle('');
-      setErrorMessage('');
-      setSuccessTitle('');
-      setSuccessMessage('');
-      setLoadingTitle('');
-      setLoadingMessage('');
-    }
-  }, [chain?.id, chain]);
+    useEffect(() => {
+        if (chain && chain!?.id != 80001) {
+            setErrorTitle("Please connect to polygon mumbai testnet");
+            setErrorMessage("Other networks are not supported currently.");
+            setSuccessTitle("");
+            setSuccessMessage("");
+            setLoadingTitle("");
+            setLoadingMessage("");
+        } else if (chain && chain!?.id == 80001) {
+            setErrorTitle("");
+            setErrorMessage("");
+            setSuccessTitle("");
+            setSuccessMessage("");
+            setLoadingTitle("");
+            setLoadingMessage("");
+        }
+    }, [chain?.id, chain]);
 
-  return (
-    <html lang='en'>
-      <body className={`${dm_sans.className} bg-darkblue`}>
-        {loadingTitle && (
-          <LoadingModal
-            loadingTitle={loadingTitle}
-            loadingMessage={loadingMessage}
-          />
-        )}
-        {errorTitle && (
-          <ErrorModal
-            errorTitle={errorTitle}
-            errorMessage={errorMessage}
-            needErrorButtonRight={true}
-            errorButtonRightText='Close'
-          />
-        )}
-        {successTitle && (
-          <SuccessModal
-            successTitle={successTitle}
-            successMessage={successMessage}
-            needSuccessButtonRight={true}
-            successButtonRightText='Done'
-          />
-        )}
-        <WagmiConfig client={client}>
-          <Navigation />
-          {children}
-          <Footer />
-        </WagmiConfig>
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body className={`${dm_sans.className} bg-darkblue`}>
+                {loadingTitle && (
+                    <LoadingModal
+                        loadingTitle={loadingTitle}
+                        loadingMessage={loadingMessage}
+                    />
+                )}
+                {errorTitle && (
+                    <ErrorModal
+                        errorTitle={errorTitle}
+                        errorMessage={errorMessage}
+                        needErrorButtonRight={true}
+                        errorButtonRightText="Close"
+                    />
+                )}
+                {successTitle && (
+                    <SuccessModal
+                        successTitle={successTitle}
+                        successMessage={successMessage}
+                        needSuccessButtonRight={true}
+                        successButtonRightText="Done"
+                    />
+                )}
+                <WagmiConfig client={client}>
+                    <Navigation />
+                    {children}
+                    <Footer />
+                </WagmiConfig>
+            </body>
+        </html>
+    );
 }
